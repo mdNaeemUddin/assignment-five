0a32e924d4cce2d7ebc329ba61613255b0cbe823
mdNaeemUddin <md.naeem_uddin98@yahoo.com>
2024-02-19 21:18:59 +0600

    done

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ for (const item of seat) {
     item.style.backgroundColor = "green";
     cnt = cnt + 1;
 
-    if (cnt >= 4) {
+    if (cnt > 4) {
       window.alert("You can buy only 4 seat");
+    } else if (cnt === 4) {
       const element = document.getElementById("discount");
       element.classList.remove("btn-disabled");
-
     } else {
       const totalSeatElement = document.getElementById("total-seat");
       const totalSeatElementText = totalSeatElement.innerText;
@@ -30,21 +30,14 @@ for (const item of seat) {
     buySeatElement.innerText = buy;
 
     const value = event.target.innerText;
-    appendFunc(value);
+    appendFunc(value, cnt);
 
     grandPrice(cnt);
     matchCoupon(cnt);
-
-    
-
-
-
   });
 }
 
 document.getElementById("success").addEventListener("click", function () {
-
-
   const item = document.getElementById("modal");
   item.classList.remove("hidden");
 
@@ -56,48 +49,41 @@ document.getElementById("success").addEventListener("click", function () {
   document.getElementById("footer").classList.add("hidden");
 });
 
+function appendFunc(item, cnt) {
+  if (cnt <= 4) {
+    const appendElement = document.getElementById("append-seat-info");
+    const div = document.createElement("div");
+    div.classList.add(
+      "flex",
+      "justify-between",
+      "items-center",
+      "text-sm",
+      "font-bold",
+      "text-[#030712]"
+    );
 
+    // div.setAttribute('id', 'seat-buy-info');
+    appendElement.appendChild(div);
+    const one = document.createElement("h3");
+    one.innerText = item;
+    div.appendChild(one);
 
-function appendFunc(item) {
-  const appendElement = document.getElementById("append-seat-info");
-  const div = document.createElement("div");
-  div.classList.add(
-    "flex",
-    "justify-between",
-    "text-sm",
-    "font-bold",
-    "text-[#030712]"
-  );
+    const two = document.createElement("h3");
+    two.innerText = "Economoy";
+    div.appendChild(two);
 
-
-  // div.setAttribute('id', 'seat-buy-info');
-  appendElement.appendChild(div);
-  const one = document.createElement("h3");
-  one.innerText = item;
-  div.appendChild(one);
-
-  const two = document.createElement("h3");
-  two.innerText = "Business Class";
-  div.appendChild(two);
-
-  const three = document.createElement("h3");
-  three.innerText = "550";
-  div.appendChild(three);
+    const three = document.createElement("h3");
+    three.innerText = "550";
+    div.appendChild(three);
+  }
 }
 
-
-
-function grandPrice(item) {
-  const price = document.getElementById("grand-price");
-  const value = 550;
-  const total = value * item;
-  price.innerText = total;
+function grandPrice(cnt) {
+  if (cnt <= 4) {
+    const price = document.getElementById("grand-price");
+    const value = 550;
+    const total = value * cnt;
+    price.innerText = total;
+  }
 }
-
-
-// function clickData(val) {
-//   const click = val;
-//   return click;
-// }
-
 
